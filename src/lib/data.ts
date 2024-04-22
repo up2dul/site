a07e902e644029eed26cf150587682a03f3388b9
Abdul <malik301002@gmail.com>
2024-04-22 17:46:14 +0700

    refactor: rename data file and add contact links data

---
+++ src/lib/data.ts
@@ -0,0 +1,38 @@
+export const site = {
+  siteName: 'Abdul Malik',
+  siteUrl: 'https://up2dul.dev',
+  github: 'https://github.com/up2dul',
+  linkedin: 'https://linkedin.com/in/up2dul',
+  cv: 'https://read.cv/up2dul',
+  email: 'up2dul@gmail.com',
+  description:
+    'A digital garden (personal website) by Abdul Malik, a Frontend Web Developer based in Indonesia.',
+  keywords: ['personal', 'website', 'blog', 'article', 'portfolio'],
+};
+
+export const contacts = [
+  {
+    name: 'Email',
+    url: 'mailto:up2dul@gmail.com',
+  },
+  {
+    name: 'CV',
+    url: 'https://read.cv/up2dul',
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/up2dul',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://linkedin.com/in/up2dul',
+  },
+  {
+    name: 'Telegram',
+    url: 'https://t.me/up2dul',
+  },
+  {
+    name: 'Facebook',
+    url: 'https://facebook.com/up2dul',
+  },
+];
