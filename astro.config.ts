dc358fe2de992c61225cc25c0c2d080b6b02fee9
Abdul <malik301002@gmail.com>
2024-04-29 10:00:45 +0700

    feat: sitemap configurations
    Edit astro configuration, add robots.txt along with the Sitemap URL, and
    add sitemap head link

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://up2dul.dev',
   integrations: [
     tailwind(),
+    sitemap(),
     icon({
       include: {
         tabler: ['*'],
