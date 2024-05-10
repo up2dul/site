5816f319d7ed6d9f900e0f333647013c9a6735f0
Abdul <malik301002@gmail.com>
2024-05-10 14:07:01 +0700

    feat: add best-practices assertions
    And set it to warning with minimal score to 0.9

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -8,6 +8,7 @@ module.exports = {
       assertions: {
         'categories:performance': ['error', { minScore: 0.9 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
         'categories:seo': ['warn', { minScore: 0.9 }],
       },
     },
