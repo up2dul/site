1562c001414b4c38b32d9e13f15ac8b0d358e4be
Abdul <malik301002@gmail.com>
2024-05-19 04:03:03 +0700

    feat(comps): create WIP component (temporary)
    Used for tell user if the site is still under development (even it's not
    really necessary)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,5 +2,6 @@ import ContentCard from './ContentCard.astro';
 import Link from './Link.astro';
 import SkipToContent from './SkipToContent.astro';
 import ThemeButton from './ThemeButton.astro';
+import WIP from './WIP.astro';
 
-export { ContentCard, Link, SkipToContent, ThemeButton };
+export { ContentCard, Link, SkipToContent, ThemeButton, WIP };
