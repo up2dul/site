4c6a2e073e08be5e58418830b8d3532149aab638
abdul <up2dul@gmail.com>
2025-02-14 14:48:49 +0700

    feat: create hero pattern section

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,8 @@
 import ContentCard from './ContentCard.astro';
+import HeroPattern from './HeroPattern.astro';
 import Link from './Link.astro';
 import SkipToContent from './SkipToContent.astro';
 import ThemeButton from './ThemeButton.astro';
 import WIP from './WIP.astro';
 
-export { ContentCard, Link, SkipToContent, ThemeButton, WIP };
+export { ContentCard, HeroPattern, Link, SkipToContent, ThemeButton, WIP };
